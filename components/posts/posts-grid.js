8583d11bb673dab5e60b27dd8f77fe16c4b06d35
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-03 16:43:16 +0700

    preparing posts component

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,16 @@
+import PostItem from './post-item';
+import classes from './posts-grid.module.css';
+
+const PostsGrid = ({ posts }) => {
+  return (
+    <>
+      <ul className={classes.grid}>
+        {posts.map((post) => (
+          <PostItem />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PostsGrid;
