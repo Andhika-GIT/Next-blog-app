f2e1af3402da629aa67e6807e00edbc6f14ffcd8
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-05 14:37:31 +0700

    change the dummy data slug

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Hero from '../components/home-page/hero';
 
 const DUMMY_POSTS = [
   { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
-  { slug: 'getting-started-with-nextjs2', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
-  { slug: 'getting-started-with-nextjs3', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
 ];
 
 const HomePage = () => {
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,13 @@
+import AllPosts from '../../components/posts/all-posts';
+
 const DUMMY_POSTS = [
   { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
-  { slug: 'getting-started-with-nextjs2', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
-  { slug: 'getting-started-with-nextjs3', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
 ];
 
 const AllPostsPage = () => {
-  return <AllPostsPage posts={DUMMY_POSTS} />;
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
