1e64b6824feaf42cb7422b91f067abe7168016f9
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-05 14:35:03 +0700

    All posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,11 @@
-const AllPostsPage = () => {};
+const DUMMY_POSTS = [
+  { slug: 'getting-started-with-nextjs', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs2', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+  { slug: 'getting-started-with-nextjs3', title: 'Getting started with Nextjs', image: 'getting-started-nextjs.png', excerpt: 'NextJS is react framework for production', date: '2022-02-10' },
+];
+
+const AllPostsPage = () => {
+  return <AllPostsPage posts={DUMMY_POSTS} />;
+};
 
 export default AllPostsPage;
