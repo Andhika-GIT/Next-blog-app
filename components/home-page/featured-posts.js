413002697cb007c36a8be4c5d9036d178ea3b039
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-05 10:22:00 +0700

    setup the post dummy data

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,9 +1,11 @@
+import PostsGrid from '../posts/posts-grid';
 import classes from './featured-posts.module.css';
 
-const FeaturedPosts = () => {
+const FeaturedPosts = ({ posts }) => {
   return (
     <section>
       <h2>Featured Posts</h2>
+      <PostsGrid posts={posts} />
     </section>
   );
 };
